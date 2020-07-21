c1f274baddfc12497d720573f9cf1860c39428b7
Chantal St Louis <misscstlouis@gmail.com>
2020-07-21 22:07:15 +0100

    Adds About page content

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function About() {
-  return <h1>hi from about page</h1>;
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore recusandae tempora provident laborum, laudantium natus ipsa dolorum eveniet hic repellat soluta a ullam aperiam saepe maiores aliquid impedit beatae unde totam consequatur blanditiis similique sit. Sequi aut quisquam aperiam hic!</p>
+    </section>
+  );
 }
