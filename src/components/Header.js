0830c33d0bbfeb1ef33b567042de8c80d29bbc26
Chantal St Louis <misscstlouis@gmail.com>
2020-07-21 21:51:39 +0100

    Adds basic setup of Routing and adds Header component to project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { Link } from 'react-router-dom'
+
+import logo from '../assets/logo.svg'
 
 export default function Header() {
-  return <h1>hello from header</h1>;
+  return (
+    <header className="header">
+      <img src={logo} alt="Vintage Tech Logo" className='logo' />
+    </header>
+    );
 }
