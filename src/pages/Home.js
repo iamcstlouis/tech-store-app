02f405a43b547667c870f7b37c3c2ca7373ba6ab
Chantal St Louis <misscstlouis@gmail.com>
2020-07-21 22:15:24 +0100

    Adds Hero component to Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { Link } from 'react-router-dom'
+
+import Hero from '../components/Hero'
 
 export default function Home() {
-  return <h1>hello from home page</h1>;
+  return (
+    <>
+      <Hero>
+        <Link to='/products' className="btn btn-primary btn-hero"> Our Products</Link>
+      </Hero>
+    </>
+  );
 }
