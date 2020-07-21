9a0b1cd8de159ee486fd4dc371b20bc21ac29773
Chantal St Louis <misscstlouis@gmail.com>
2020-07-21 21:58:54 +0100

    Sets up Links in Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,29 @@ export default function Header() {
   return (
     <header className="header">
       <img src={logo} alt="Vintage Tech Logo" className='logo' />
+      <nav>
+        <ul>
+          <div>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/products'>Products</Link>
+            </li>
+          </div>
+          <div>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+            <li>
+              <Link to='/cart'>Cart</Link>
+            </li>
+          </div>
+        </ul>
+      </nav>
     </header>
-    );
+  );
 }
