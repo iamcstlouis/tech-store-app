c1269c2565d6ef4beaa64a98571533295be9ab3f
Chantal St Louis <misscstlouis@gmail.com>
2020-07-23 15:43:02 +0100

    Adds basic context setup for products

--- src/context/products.js
+++ src/context/products.js
@@ -1,1 +1,19 @@
-// products context
+import React from 'react'
+
+export const ProductContext = React.createContext();
+
+// Provider, Consumer, useContext()
+
+export default function ProductProvider({ children }) {
+
+    const [loading, setLoading] = React.useState(false);
+    const [products, setProducts] = React.useState([]);
+    const [featured, setfeatured] = React.useState([]);
+
+    return (
+        <ProductContext.Provider value={{ loading, products, featured }}>
+            {children}
+        </ProductContext.Provider>
+    )
+}
+
--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import ProductProvider from './context/products'
+
+ReactDOM.render(
+    <ProductProvider>
+        <App />
+    </ProductProvider>,
+    document.getElementById("root"));
