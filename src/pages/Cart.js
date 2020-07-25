c96bdeeb26f025ec8527aecb0f9d3a98225298d8
Chantal St Louis <misscstlouis@gmail.com>
2020-07-25 18:29:32 +0100

    Sets up EmptyCart component

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
 import { CartContext } from '../context/cart'
+// import { UserContext } from '../context/user'
+import EmptyCart from '../components/Cart/EmptyCart'
+import CartItem from '../components/Cart/CartItem'
 
 export default function Cart() {
+  let user = false;
   const { cart, total } = React.useContext(CartContext)
-  console.log({ cart, total })
+
+  if (cart.length === 0) {
+    return <EmptyCart />
+  }
+
   return <h1>hello from cart page</h1>;
 }
